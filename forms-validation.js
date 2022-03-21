bcf4959c1fa836829422bdde63df11536805da00
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-03-21 09:07:38 +0200

    Update Wait for document load and some css

--- forms-validation.js
+++ forms-validation.js
@@ -1,57 +1,61 @@
-window.addEventListener('message', event => {
-	if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady') {
-		let error_messages = {
-			firstname: 'Please provide a name',
-			email: 'Please provide a properly formatted email address',
-			website: 'Please provide a properly formatted website address',
-			checkbox_field: 'Please agree that you\'re totally awesome',
-			complete_all_fields: 'Please complete all required fields.'
-		}
+document.addEventListener('DOMContentLoaded', function(event) {
 
-		let input = document.querySelectorAll('input'), 
-				submit = document.querySelector('form input[type=submit]')
+	window.addEventListener('message', event => {
+		if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady') {
+			let error_messages = {
+				firstname: 'Please provide a name',
+				email: 'Please provide a properly formatted email address',
+				website: 'Please provide a properly formatted website address',
+				checkbox_field: 'Please agree that you\'re totally awesome',
+				complete_all_fields: 'Please complete all required fields.'
+			}
 
-		function onChangeHandler(event) {
-			if (!event) event = window.event;
-			var changedElement = event.srcElement || this;
-			setTimeout(function(){  
-				if( changedElement.classList.contains('invalid') || changedElement.classList.contains('error') || changedElement.getAttribute('type') == 'checkbox' ){
-					let parentElement = changedElement.closest('.field');
-					let errorDiv = parentElement.querySelector('.hs-error-msg')
-					if(errorDiv) errorDiv.innerHTML = `<span>&#9888;</span> ${error_messages[changedElement.getAttribute('name')]}`
-				}
-			}, 100)
-		}
+			let input = document.querySelectorAll('input'), 
+					submit = document.querySelector('form input[type=submit]')
 
-		for (var i = 0; i < input.length; i += 1) {
-			let typeCheck = input[i].getAttribute('type') == 'checkbox' ? true : input[i].hasAttribute('required')
-			if( error_messages.hasOwnProperty(input[i].getAttribute('name')) && typeCheck ){
-				['keyup', 'mouseleave', 'click','mouseout', 'onfocusout'].forEach(function(e) {
-					input[i].addEventListener(e, onChangeHandler)
-				})
-			}  
-		}
+			function onChangeHandler(event) {
+				if (!event) event = window.event;
+				var changedElement = event.srcElement || this;
+				setTimeout(function(){  
+					if( changedElement.classList.contains('invalid') || changedElement.classList.contains('error') || changedElement.getAttribute('type') == 'checkbox' ){
+						let parentElement = changedElement.closest('.field');
+						let errorDiv = parentElement.querySelector('.hs-error-msg')
+						if(errorDiv) errorDiv.innerHTML = `<span>&#9888;</span> ${error_messages[changedElement.getAttribute('name')]}`
+					}
+				}, 100)
+			}
 
-		submit.addEventListener('click', function(e) {
 			for (var i = 0; i < input.length; i += 1) {
 				let typeCheck = input[i].getAttribute('type') == 'checkbox' ? true : input[i].hasAttribute('required')
 				if( error_messages.hasOwnProperty(input[i].getAttribute('name')) && typeCheck ){
-					let changedElement = input[i];
-					setTimeout(function(){  
-						if( changedElement.classList.contains('invalid') || changedElement.classList.contains('error') || changedElement.getAttribute('type') == 'checkbox' ){
-							let parentElement = changedElement.closest('.field');
-							let errorDiv = parentElement.querySelector('.hs-error-msg')
-							if(errorDiv) errorDiv.innerHTML = `<span>&#9888;</span> ${error_messages[changedElement.getAttribute('name')]}`
-						}
-
-						let complete_all_fields = document.querySelector('.hs_error_rollup label.hs-main-font-element');
-						if( document.body.contains( complete_all_fields ) ){
-							complete_all_fields.innerHTML = `<span>&#9888;</span> ${error_messages['complete_all_fields']}`
-						}
-					}, 50)
+					['keyup', 'mouseleave', 'click','mouseout', 'onfocusout'].forEach(function(e) {
+						input[i].addEventListener(e, onChangeHandler)
+					})
 				}  
 			}
-		})
 
-	}
+			submit.addEventListener('click', function(e) {
+				for (var i = 0; i < input.length; i += 1) {
+					let typeCheck = input[i].getAttribute('type') == 'checkbox' ? true : input[i].hasAttribute('required')
+					if( error_messages.hasOwnProperty(input[i].getAttribute('name')) && typeCheck ){
+						let changedElement = input[i];
+						setTimeout(function(){  
+							if( changedElement.classList.contains('invalid') || changedElement.classList.contains('error') || changedElement.getAttribute('type') == 'checkbox' ){
+								let parentElement = changedElement.closest('.field');
+								let errorDiv = parentElement.querySelector('.hs-error-msg')
+								if(errorDiv) errorDiv.innerHTML = `<span>&#9888;</span> ${error_messages[changedElement.getAttribute('name')]}`
+							}
+
+							let complete_all_fields = document.querySelector('.hs_error_rollup label.hs-main-font-element');
+							if( document.body.contains( complete_all_fields ) ){
+								complete_all_fields.innerHTML = `<span>&#9888;</span> ${error_messages['complete_all_fields']}`
+							}
+						}, 50)
+					}  
+				}
+			})
+
+		}
+	})
+	
 })
